3eed423b5c7aaa1b20c4f47009eeb3d3d03d04c7
Edward O <edwardoreilly0906@gmail.com>
2025-04-16 16:45:06 -0500

    Add GraphQL mutations for user login and registration, and a query to fetch user details

---
+++ client/src/graphql/mutations.ts
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`; 
